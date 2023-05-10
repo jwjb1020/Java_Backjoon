09a81869eec08b14eb07791bf2eef27d2ecafd15
jwjb1020 <jwjb1020@gmail.com>
2023-05-10 19:59:44 +0900

    2023.05.10

--- Backjoon/반복문/ab4.java
+++ Backjoon/반복문/ab4.java
@@ -2,20 +2,21 @@ package Backjoon.반복문;
 
 import java.util.Scanner;
 
-public class ab5 {
+public class ab4 {
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int sum = 0;
-		while(true) {
+		while(sc.hasNextInt()) {
 			int a = sc.nextInt();
 			int b = sc.nextInt();
 			sum = a+b;
-			if (sum == 0)
-				break;
-			System.out.println(a+b);
+			
+			System.out.println(sum);
 			
 		}
 		sc.close();
+
 	}
 
 }
